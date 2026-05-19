9b2438ef97af1d9ff9f904a31b44bd924b8b70a1
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-19 18:37:10 -0300

    feat: Cria o InMemoryAccountRepository, que implementa a interface e gerencia o banco em memoria com o ConcurrentHashMap para lidar com multi threads

---
+++ src/main/java/br/com/gabrieltiziano/ipkiss/repository/InMemoryAccountRepository.java
@@ -0,0 +1,29 @@
+package br.com.gabrieltiziano.ipkiss.repository;
+
+import br.com.gabrieltiziano.ipkiss.domain.model.Account;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@Repository
+public class InMemoryAccountRepository implements AccountRepository{
+    private final ConcurrentMap<String, Account> accounts = new ConcurrentHashMap<>();
+
+    @Override
+    public Optional<Account> findById(String id) {
+        return Optional.ofNullable(accounts.get(id));
+    }
+
+    @Override
+    public Account save(Account account) {
+        accounts.put(account.getId(), account);
+        return account;
+    }
+
+    @Override
+    public void clear() {
+        accounts.clear();
+    }
+}
