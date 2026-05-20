041c9b55fb369ac12a6658b431d56049d5be75c6
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-20 12:09:03 -0300

    feat: Adiciona o caso de TransferRequest como tipo do event ao EventController

--- src/main/java/br/com/gabrieltiziano/ipkiss/web/EventController.java
+++ src/main/java/br/com/gabrieltiziano/ipkiss/web/EventController.java
@@ -2,6 +2,8 @@ package br.com.gabrieltiziano.ipkiss.web;
 
 import br.com.gabrieltiziano.ipkiss.domain.model.Account;
 import br.com.gabrieltiziano.ipkiss.domain.service.DepositService;
+import br.com.gabrieltiziano.ipkiss.domain.service.TransferResult;
+import br.com.gabrieltiziano.ipkiss.domain.service.TransferService;
 import br.com.gabrieltiziano.ipkiss.domain.service.WithdrawService;
 import br.com.gabrieltiziano.ipkiss.web.dto.*;
 import org.springframework.http.HttpStatus;
@@ -14,10 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class EventController {
     private final DepositService depositService;
     private final WithdrawService withdrawService;
+    private final TransferService transferService;
 
-    public EventController(DepositService depositService, WithdrawService withdrawService) {
+    public EventController(DepositService depositService, WithdrawService withdrawService, TransferService transferService) {
         this.depositService = depositService;
         this.withdrawService = withdrawService;
+        this.transferService = transferService;
     }
 
     @PostMapping("/event")
@@ -32,6 +36,13 @@ public class EventController {
             Account result = withdrawService.withdraw(withdrawRequest.getOrigin(), withdrawRequest.getAmount());
             AccountResponse accountResponse = new AccountResponse(result.getId(), result.getBalance());
             return ResponseEntity.status(HttpStatus.CREATED).body(new WithdrawResponse(accountResponse));
+        }
+
+        if (event instanceof  TransferRequest transferRequest){
+            TransferResult result = transferService.transfer(transferRequest.getOrigin(), transferRequest.getAmount(), transferRequest.getDestination());
+            AccountResponse accountResponseOrigin = new AccountResponse(result.origin().getId(), result.origin().getBalance());
+            AccountResponse accountResponseDestination = new AccountResponse(result.destination().getId(), result.destination().getBalance());
+            return ResponseEntity.status(HttpStatus.CREATED).body(new TransferResponse(accountResponseOrigin, accountResponseDestination));
         }
 
         throw new IllegalArgumentException("Unsupported event type");
